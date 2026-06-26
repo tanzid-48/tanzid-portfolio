5caf2b857ca94ae9225ae279e7c3af98bec695e8
tanzid-48 <mdtanzid.525@gmail.com>
2026-06-26 14:10:55 +0600

    feat: add Lenis smooth scroll to portfolio

--- app/page.js
+++ app/page.js
@@ -1,20 +1,21 @@
 import About from "./components/About ";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
+import LenisScroll from "./components/LenisScroll";
 import Navbar from "./components/Navbar";
 import Services from "./components/Services";
 import Work from "./components/Work";
 
-
 export default function Home() {
   return (
     <>
+      <LenisScroll />
       <Navbar />
       <Header />
-      <About/>
-      <Services/>
-      <Work/>
-      <Contact/>
+      <About />
+      <Services />
+      <Work />
+      <Contact />
     </>
   );
 }
